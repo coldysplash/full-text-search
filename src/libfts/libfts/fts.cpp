07278588b04d4a78b19da6061ab851819ed284c6
Ivan <ivanreshetnikov235@gmail.com>
2023-09-05 19:29:14 +0700

    Add function sum_two_numbers

---
+++ src/libfts/libfts/fts.cpp
@@ -0,0 +1,9 @@
+#include <libfts/fts.hpp>
+
+namespace fts{
+
+double sum_two_numbers(double x, double y){
+    return x + y;
+}
+
+}
---
+++ src/libfts/libfts/fts.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace fts{
+
+double sum_two_numbers(double x, double y);
+
+}
