d83244efa6318b9946bbfb5777b9fc6fb1369755
Ivan <ivanreshetnikov235@gmail.com>
2023-09-05 22:02:07 +0700

    Create main app

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -1,7 +1,24 @@
-#include <libfts/fts.hpp>
 #include <iostream>
 
-int main(){
+#include <libfts/fts.hpp>
+
+#include <CLI/CLI.hpp>
+
+#include <string>
+
+int main(int argc, char **argv) {
+
+    CLI::App app("Sum two numbers");
+
+    double value_1{0.0};
+    double value_2{0.0};
+    app.add_option("--first", value_1, "./(app) --first x --second y for sum two numbers");
+    app.add_option("--second", value_2, "(app) - name exe file | (x, y) - value numbers");
+    
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "Sum = " << fts::sum_two_numbers(value_1, value_2) << std::endl;
 
     return 0;
 }
