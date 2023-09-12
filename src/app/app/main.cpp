d34b4e2ff43a4cc2799e303d1f8a1039b8beb891
Ivan <ivanreshetnikov235@gmail.com>
2023-09-12 21:25:27 +0700

    Add functions: delete_puncts, str_tolower, delete_spaces, delete_stop_words

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -6,11 +6,24 @@
 
 #include <string>
 
+#include <vector>
+
 // int main(int argc, char **argv) {
 int main() {
-  std::string text = "H.e.l.l.o!";
+  std::string text = "Hello and goodbye!!!";
   parser::delete_puncts(text);
-  std::cout << text << std::endl;
+  parser::str_tolower(text);
+  std::vector<std::string> list_strings;
+  parser::delete_spaces(list_strings, text);
+  std::vector<std::string> stop_words = {"and", "a", "an"};
+  parser::delete_stop_words(list_strings, stop_words);
+  for(auto &s: list_strings){
+    std::cout << s << '\n';
+  }
+  // const std::vector<std::string> stop_words = {"and", "a" , "an"};
+  // parser::delete_stop_words(text, stop_words);
+
+  // std::cout << text << std::endl;
 
   // CLI::App app("Sum two numbers");
 
--- src/common/common/parser.hpp
+++ src/common/common/parser.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include <string>
+#include <vector>
 
 namespace parser {
 
-void delete_puncts(std::string &text);
+void delete_puncts(std::string &pars_string);
+void str_tolower(std::string &pars_string);
+void delete_spaces(std::vector<std::string> &list_strings, std::string &pars_string);
+void delete_stop_words(
+    std::vector<std::string> &list_strings, const std::vector<std::string> &stop_words);
 
 } // namespace parser
